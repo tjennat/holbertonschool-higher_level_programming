983e4731612bc3ba9b989c3e75d2312d35a75826
tjennat <theo.jennat98@gmail.com>
2024-01-22 17:00:54 +0100

    changing synthax

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = "object-oriented programming with " + str[:6]
-print(str)
+print(str[39:66], str[107:111], str[0:6])
+# required line
