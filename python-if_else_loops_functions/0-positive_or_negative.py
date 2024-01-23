2402ee9aee3fe786844d9d143a7ead174877f598
tjennat <theo.jennat98@gmail.com>
2024-01-23 12:25:08 +0100

    function that checks if the random generated number is positiv negativ or equal to 0

---
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import random
+
+number = random.randint(-10, 10)
+
+if number > 0:
+    print("{} is positive".format(number))
+
+if number == 0:
+    print("{} is zero".format(number))
+
+if number < 0:
+    print("{} is negative".format(number))
