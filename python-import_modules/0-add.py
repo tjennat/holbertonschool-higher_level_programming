5b07ad3f6567b4d1e735aa388cc0154bb99a8e4f
tjennat <theo.jennat98@gmail.com>
2024-01-25 17:44:42 +0100

    fixing func

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -4,5 +4,4 @@ if __name__ == "__main__":
     from add_0 import add_0
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add_0(a + b)))
- 
+    print("{} + {} = {}".format(a, b, add_0(a, b)))
