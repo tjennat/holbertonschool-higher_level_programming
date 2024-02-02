680acbeca1c628c1d4e7820c1237026460a48f68
tjennat <theo.jennat98@gmail.com>
2024-02-02 13:39:41 +0100

    pep8 + correcting output

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for i in range(x):
+    count = 0
+    for i in range(x):
+        try:
             print(my_list[i], end="")
-        print()
-        return i + 1
-    except IndexError:
-        print()
-        return i
+            count += 1
+        except IndexError:
+            break
+    print()
+    return count
