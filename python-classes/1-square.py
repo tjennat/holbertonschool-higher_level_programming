18e2d7b0450dc689c679b7ade8f6b48c65471b16
tjennat <theo.jennat98@gmail.com>
2024-02-06 11:50:58 +0100

    incrementing an attribute in my function

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""empty class that defines a square."""
+
+
+class Square:
+    """empty class that defines a square."""
+
+
+def __init__(self):
+    self.size = 0
+    """empty instance attribute"""
+
+
+pass
