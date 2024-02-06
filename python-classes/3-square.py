658a3bd08ef3ccbc1ca3429cd9fd24c36a9657e7
tjennat <theo.jennat98@gmail.com>
2024-02-06 15:45:47 +0100

    adding area self

---
+++ python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""empty class that defines a square."""
+
+
+class Square:
+    """empty class that defines a square."""
+
+    __size = None
+
+    def __init__(self, __size=0):
+        if not isinstance(__size, int):
+            raise TypeError("size must be an integer")
+        elif __size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = __size
+    def area(self):
+        return self.__size * self.__size
