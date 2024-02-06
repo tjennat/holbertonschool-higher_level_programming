b528b47c92fc18f14ac6112e4e49273a37bca93e
tjennat <theo.jennat98@gmail.com>
2024-02-06 16:10:46 +0100

    adding value plus type error and value error

---
+++ python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""empty class that defines a square."""
+
+
+class Square:
+    """empty class that defines a square."""
+
+    __size = None
+
+    def __init__(self, __size=0):
+        if not isinstance(__size, int):
+            raise TypeError("size must be an integer")
+        elif __size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = __size
+
+    def area(self):
+        """returns the current square area"""
+
+        return self.__size * self.__size
+
+    def size(self, value):
+        """getter function for size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
