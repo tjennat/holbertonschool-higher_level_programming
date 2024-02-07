c810014435384ed1fc01f7fcde9ae660fe984b44
tjennat <theo.jennat98@gmail.com>
2024-02-07 20:11:49 +0100

    unchanged

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,28 +1,7 @@
-#!/usr/bin/python3
-"""empty class that defines a square."""
-
-
-class Square:
-    """empty class that defines a square."""
-
-    __size = None
-
-    @property
-    def size(self):
-        return self.__size
-
-    def __init__(self, __size=0):
-        if not isinstance(__size, int):
-            raise TypeError("size must be an integer")
-        elif __size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = __size
-
-    def area(self):
+def area(self):
         """returns the current square area"""
 
-        return self.__size ** 2
+        return self.__size * self.__size
 
     @size.setter
     def size(self, value):
@@ -31,7 +10,8 @@ class Square:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            self.__size = value
 
     def my_print(self):
         if self.__size == 0:
@@ -52,4 +32,3 @@ class Square:
            not all(isinstance(num, int) and num >= 0 for num in value):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
-        
