6f425bd9a9b59a4ec0cfcb8d9c4dbfef1b6850f2
tjennat <theo.jennat98@gmail.com>
2024-02-08 13:23:16 +0100

    adding a total of 9 function test

--- python-test_driven_development/0-main.py
+++ python-test_driven_development/0-main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 add_integer = __import__('0-add_integer').add_integer
 
-print(add_integer('a', 2))
+print(add_integer(5))
 print(add_integer(100, -2))
 print(add_integer(2))
 print(add_integer(100.3, -2))
