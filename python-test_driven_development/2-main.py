cc12d41f5bca6555ede2430f6909bd08816b7ada
tjennat <theo.jennat98@gmail.com>
2024-02-08 15:48:38 +0100

    final version

--- python-test_driven_development/2-main.py
+++ python-test_driven_development/2-main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
-matrix = [[]]
+matrix = [[1, 2, "e"], [4, "t", 6]]
 
 print(matrix_divided(matrix, 3))
 print(matrix)
