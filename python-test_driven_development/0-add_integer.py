405d7a86f77a64393a96bbc8ef47639ef99bcd12
tjennat <theo.jennat98@gmail.com>
2024-02-08 12:56:23 +0100

    test driven + add function

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module defines the add_integer function
+
+
+
+
+"""
+
+
+def add_integer(a, b=98):
+    """This function adds two integers
+    sum of two integers
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
