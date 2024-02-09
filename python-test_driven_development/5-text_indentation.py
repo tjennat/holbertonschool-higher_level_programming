615135d499f2f1b8997afc06464949b07c1ca4ca
tjennat <theo.jennat98@gmail.com>
2024-02-09 15:36:33 +0100

    Changing my function so that can handle the space

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -10,6 +10,9 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     for i in range(len(text)):
+        if text[i] == " " and text[i - 1] in ".?:":
+            continue
         print(text[i], end="")
-        if text[i] in ('.', '?', ':'):
+        if text[i] in ".?:":
             print('\n')
+            
