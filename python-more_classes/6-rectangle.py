12ddd271e76d4d0ef7633012289fb9054beaf143
tjennat <theo.jennat98@gmail.com>
2024-02-10 10:22:48 +0100

    PEP8 fix

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -9,7 +9,7 @@ class Rectangle:
     class of my rectangle
     """
     number_of_instances = 0
-    
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
