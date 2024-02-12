fe011936d10d1983c6179a99250e44539bca7067
tjennat <theo.jennat98@gmail.com>
2024-02-12 20:28:36 +0100

    fixing pep8

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,8 +3,7 @@
 
 
 def inherits_from(obj, a_class):
-    """Check if an object is an instance
-of a class that inherited from a_class"""
+    """Check if an object is an instance of a class that inherited a_class"""
     if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
     return False
