ce55e4d4d08e2940c7df5a81cebcec452bc41dee
tjennat <theo.jennat98@gmail.com>
2024-02-12 20:20:41 +0100

    func that check if an obj is a class or subclass

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module for is_kind_of_class method"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Method to check if an object is an instance of a class or a subclass"""
+    return isinstance(obj, a_class)
