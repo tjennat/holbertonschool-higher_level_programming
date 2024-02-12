007f85abd6e371006dfe93bcf3d2b4e1b64f3cd1
tjennat <theo.jennat98@gmail.com>
2024-02-12 20:30:02 +0100

    True and false not necessary

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,5 +5,3 @@
 def inherits_from(obj, a_class):
     """Check if an object is an instance of a class that inherited a_class"""
     if issubclass(type(obj), a_class) and type(obj) is not a_class:
-        return True
-    return False
