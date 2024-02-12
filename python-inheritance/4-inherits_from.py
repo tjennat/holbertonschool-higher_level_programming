7a4d7ac8c1d96bb3c7405e208019232e5ff51397
tjennat <theo.jennat98@gmail.com>
2024-02-12 20:35:25 +0100

    fixing last detail

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,7 +1,7 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 """Module for inherits_from function."""
 
 
 def inherits_from(obj, a_class):
     """Check if an object is an instance of a class that inherited a_class"""
-    return issubclass(type(obj), a_class) and type(obj) is not a_class
+    return issubclass(type(obj), a_class) and type(obj) != a_class
