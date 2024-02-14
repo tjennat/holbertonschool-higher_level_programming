b87c1a7b3f6a77a2def7aa989c9d0677967e59ec
tjennat <theo.jennat98@gmail.com>
2024-02-14 15:41:49 +0100

    same as last exercize but it is for append

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Reads a text file and prints number of char"""
+
+
+def append_write(filename="", text=""):
+     """Write a function that appends a string at the end of a text file 
+(UTF8) and returns the number of characters added:"""
+     with open(filename, "a") as f:
+          return f.write(text)
