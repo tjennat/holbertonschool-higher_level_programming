222167905ef4c392781bd84dcc4ba52962561104
tjennat <theo.jennat98@gmail.com>
2024-02-14 17:36:28 +0100

    using correct var

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,4 +6,4 @@ def save_to_json_file(my_obj, filename):
     """function that returns the JSON representation of an object (string)"""
     import json
     with open(filename, 'w') as file:
-        json.dump(my_obj, filename)
+        json.dump(my_obj, file)
