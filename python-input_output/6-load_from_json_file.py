6be7cc8ab0e922c80e9665e8853d3bb4851470ea
tjennat <theo.jennat98@gmail.com>
2024-02-14 18:34:26 +0100

    changing def

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -2,7 +2,7 @@
 """JSON object from JSON file"""
 
 
-def save_to_json_file(my_obj, filename):
+def load_from_json_file(my_obj, filename):
     """function that creates an object from a JSON file"""
     import json
     with open(filename, 'r') as file:
