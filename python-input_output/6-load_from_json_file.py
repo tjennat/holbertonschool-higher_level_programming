3e51ccc383128a625eb54bfb1ac5e25077472960
tjennat <theo.jennat98@gmail.com>
2024-02-14 18:47:30 +0100

    correcting r

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -5,5 +5,5 @@
 def load_from_json_file(my_obj, filename):
     """function that creates an object from a JSON file"""
     import json
-    with open(filename, 'r') as file:
+    with open(filename, "r") as file:
         return json.load(file)
