11b2f0452b8050b62bf4e45657fcfaad8ae29f1f
tjennat <theo.jennat98@gmail.com>
2024-02-14 14:38:55 +0100

    Function that reads a text file and print it to stdout

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+
+#!/usr/bin/python3
+"""Reads a text file and prints it to stdout"""
+
+
+def read_file(filename=""):
+    """Reads a text file and prints it to stdout"""
+    with open(filename, "r") as fobj:
+        text = fobj.read()
+    print(text, end="")
