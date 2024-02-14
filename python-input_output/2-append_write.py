ebd1b0132a56bb8d94a4e6c7a57d976c75109abc
tjennat <theo.jennat98@gmail.com>
2024-02-14 15:43:42 +0100

    fixing pycodestyle

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,7 +3,7 @@
 
 
 def append_write(filename="", text=""):
-     """Write a function that appends a string at the end of a text file 
-(UTF8) and returns the number of characters added:"""
-     with open(filename, "a") as f:
-          return f.write(text)
+    """Write a function that appends a string at the end of a text file
+and returns the number of characters added:"""
+    with open(filename, "a") as f:
+        return f.write(text)
