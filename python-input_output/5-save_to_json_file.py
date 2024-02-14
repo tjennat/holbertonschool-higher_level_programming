ad6c58fbc0743106be89a3b8423431c4582b1de6
tjennat <theo.jennat98@gmail.com>
2024-02-14 17:28:36 +0100

    changing indentation

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,4 +6,5 @@ def save_to_json_file(my_obj, filename):
     """function that returns the JSON representation of an object (string)"""
     import json
     with open(filename, 'w'):
-        return json.dump(my_obj, filename)
+        json.dump(my_obj, filename)
+    return
