61152c16147f3db2a9fbea1573e8764c4fea00f1
tjennat <theo.jennat98@gmail.com>
2024-02-20 20:26:41 +0100

    returning back in time

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -3,78 +3,14 @@
 from models.rectangle import Rectangle
 
 
-class Base:
-    def __init__(self, id=None):
-        self.id = id
-
-class Rectangle(Base):
-    def __init__(self, width, height, x=0, y=0, id=None):
-        super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
-
-    @property
-    def width(self):
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        self.__width = value
-
-    @property
-    def height(self):
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
-        self.__height = value
-
-    @property
-    def x(self):
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        if not isinstance(value, int):
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
-        self.__x = value
-
-    @property
-    def y(self):
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        if not isinstance(value, int):
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
-        self.__y = value
-
-    def area(self):
-        return self.__width * self.__height
-
 class Square(Rectangle):
+    """Class that defines a square."""
+
     def __init__(self, size, x=0, y=0, id=None):
+        """Constructor for the square class."""
         super().__init__(size, size, x, y, id)
 
-    @property
-    def size(self):
-        return self.width
-
-    @size.setter
-    def size(self, value):
-        self.width = value
-        self.height = value
+    def __str__(self):
+        """String representation of the square."""
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width)
