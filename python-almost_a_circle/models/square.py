165d75041a7877ab603fb5bb87372979c08fd5f5
tjennat <theo.jennat98@gmail.com>
2024-02-20 19:43:43 +0100

    a new square that inherits from rectangle adding init and str

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""importing rectangle for Square class."""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Class that defines a square."""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Constructor for the square class."""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """String representation of the square."""
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width)
