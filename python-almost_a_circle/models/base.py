4a67781bb86fccdd4ceec53aeff221519bb66702
tjennat <theo.jennat98@gmail.com>
2024-02-22 00:04:23 +0100

    adding dictionnary

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -39,3 +39,12 @@ class Base:
         if json_string is None or len(json_string) == 0:
             return []
         return json.loads(json_string)
+    
+    @classmethod
+    def create(cls, **dictionary):
+        if cls.__name__ == "Rectangle":
+            new = cls(1, 1)
+        if cls.__name__ == "Square":
+            new = cls(1)
+        new.update(**dictionary)
+        return new
