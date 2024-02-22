a2a2c2136c51fc70acb363f2517dd9d8edb3b125
tjennat <theo.jennat98@gmail.com>
2024-02-22 17:08:24 +0100

    all unittest

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -1,294 +1,132 @@
 #!/usr/bin/python3
-'''Module for Rectangle unit tests.'''
+"""Rectangle class test"""
+
+
 import unittest
-import json
-import sys
-import os
-
-
-from io import StringIO
-from models.base import Base
 from models.rectangle import Rectangle
-from models.square import Square
 
 
-class TestRectangle(unittest.TestCase):
-    """Tests the Rectangle class."""
-    def test_initialisation(self):
-        """Initialise the rectangle"""
-        r1 = Rectangle(1, 2)
+class TestBase(unittest.TestCase):
+    def test_1(self):
+        Rectangle(1, 2)
 
-        self.assertIsInstance(r1, Rectangle)
-        self.assertEqual(r1.width, 1)
-        self.assertEqual(r1.height, 2)
+    def test_2(self):
+        Rectangle(1, 2, 3)
 
-        r2 = Rectangle(1, 2, 3)
+    def test_3(self):
+        Rectangle(1, 2, 3, 4)
 
-        self.assertIsInstance(r2, Rectangle)
-        self.assertEqual(r2.width, 1)
-        self.assertEqual(r2.height, 2)
-        self.assertEqual(r2.x, 3)
+    def test_4(self):
+        with self.assertRaises(TypeError) as context:
+            Rectangle("1", 2)
+        self.assertTrue('width must be an integer' in str(context.exception))
 
-        r3 = Rectangle(1, 2, 3, 4)
+    def test_5(self):
+        with self.assertRaises(TypeError) as context:
+            Rectangle(1, "2")
+        self.assertTrue('height must be an integer' in str(context.exception))
 
-        self.assertIsInstance(r3, Rectangle)
-        self.assertEqual(r3.width, 1)
-        self.assertEqual(r3.height, 2)
-        self.assertEqual(r3.x, 3)
-        self.assertEqual(r3.y, 4)
+    def test_6(self):
+        with self.assertRaises(TypeError) as context:
+            Rectangle(1, 2, "3")
+        self.assertTrue('x must be an integer' in str(context.exception))
 
-        with self.assertRaises(TypeError):
-            r4 = Rectangle("1", 2)
-        
-        with self.assertRaises(TypeError):
-            r5 = Rectangle(1, "2")
+    def test_7(self):
+        with self.assertRaises(TypeError) as context:
+            Rectangle(1, 2, 3, "4")
+        self.assertTrue('y must be an integer' in str(context.exception))
 
-        with self.assertRaises(TypeError):
-            r6 = Rectangle(1, 2, "3")
+    def test_8(self):
+        Rectangle(1, 2, 3, 4, 5)
 
-        with self.assertRaises(TypeError):
-            r7 = Rectangle(1, 2, 3, "4")
+    def test_9(self):
+        with self.assertRaises(ValueError) as context:
+            Rectangle(-1, 2)
+        self.assertTrue('width must be > 0' in str(context.exception))
 
-        r8 = Rectangle(1, 2, 3, 4, 5)
+    def test_10(self):
+        with self.assertRaises(ValueError) as context:
+            Rectangle(1, -2)
+        self.assertTrue('height must be > 0' in str(context.exception))
 
-        self.assertIsInstance(r8, Rectangle)
-        self.assertEqual(r8.width, 1)
-        self.assertEqual(r8.height, 2)
-        self.assertEqual(r8.x, 3)
-        self.assertEqual(r8.y, 4)
-        self.assertEqual(r8.id, 5)
+    def test_11(self):
+        with self.assertRaises(ValueError) as context:
+            Rectangle(0, 2)
+        self.assertTrue('width must be > 0' in str(context.exception))
 
-        with self.assertRaises(ValueError):
-            r9 = Rectangle(-1, 2)
+    def test_12(self):
+        with self.assertRaises(ValueError) as context:
+            Rectangle(1, 0)
+        self.assertTrue('height must be > 0' in str(context.exception))
 
-        with self.assertRaises(ValueError):
-            r10 = Rectangle(1, -2)
+    def test_13(self):
+        with self.assertRaises(ValueError) as context:
+            Rectangle(1, 2, -3)
+        self.assertTrue('x must be >= 0' in str(context.exception))
 
-        with self.assertRaises(ValueError):
-            r11 = Rectangle(0, 2)
+    def test_14(self):
+        with self.assertRaises(ValueError) as context:
+            Rectangle(1, 2, 3, -4)
+        self.assertTrue('y must be >= 0' in str(context.exception))
 
-        with self.assertRaises(ValueError):
-            r12 = Rectangle(1, 0)
+    def test_15(self):
+        Rectangle.create(**{'width': 2, 'height': 3})
 
-        with self.assertRaises(ValueError):
-            r13 = Rectangle(1, 2, -3)
+    def test_16(self):
+        Rectangle.create(**{'width': 2, 'height': 3, 'x': 12})
 
-        with self.assertRaises(ValueError):
-            r14 = Rectangle(1, 2, 3, -4)
+    def test_17(self):
+        Rectangle.create(**{'width': 2, 'height': 3, 'x': 12, 'y': 1})
 
-    def test_area(self):
-        """Test the area of rectangle"""
-        r1 = Rectangle(1, 2, 3, 4)
-        size_area = r1.area()
+    def test_18(self):
+        Rectangle.create(**{'width': 2, 'height': 3,
+                         'x': 12, 'y': 1, 'id': 89})
 
-        self.assertEqual(size_area, 2)
+    def test_19(self):
+        Rectangle.load_from_file()
 
-    def test__str__(self):
-        """Test __str__"""
-        r1 = Rectangle(4, 6, 2, 1, 12)
-        expected_output = '[Rectangle] (12) 2/1 - 4/6\n'
-    
-        original_stdout = sys.stdout
-        sys.stdout = StringIO()
-    
-        print(r1)
-        print_output = sys.stdout.getvalue()
+    def test_20(self):
+        Rectangle.load_from_file()
 
-        sys.stdout = original_stdout
-        self.assertEqual(print_output, expected_output)
+    def test_21(self):
+        r1 = Rectangle(8, 65, 2, 10, 2)
+        r2 = Rectangle(10, 2, 1, 3, 5)
+        Rectangle.save_to_file([r1, r2])
 
-    def test_display_without_x_y(self):
-        """Test display() whihout x and y"""
-        r1 = Rectangle(1, 2)
-        expected_output = "#\n#\n"
-
-        original_stdout = sys.stdout
-        sys.stdout = StringIO()
-
-        r1.display()
-        displayed_output = sys.stdout.getvalue()
-
-        sys.stdout = original_stdout
-        self.assertEqual(displayed_output, expected_output)
-
-    def test_display_without_y(self):
-        """Test display() whitout y"""
-        r1 = Rectangle(1, 2, 1)
-        expected_output = " #\n #\n"
-
-        original_stdout = sys.stdout
-        sys.stdout = StringIO()
-
-        r1.display()
-        displayed_output = sys.stdout.getvalue()
-
-        sys.stdout = original_stdout
-        self.assertEqual(displayed_output, expected_output)
-
-    def test_display(self):
-        """Test display() whitout y"""
-        r1 = Rectangle(1, 2, 1, 1)
-        expected_output = "\n #\n #\n"
-
-        original_stdout = sys.stdout
-        sys.stdout = StringIO()
-
-        r1.display()
-        displayed_output = sys.stdout.getvalue()
-
-        sys.stdout = original_stdout
-        self.assertEqual(displayed_output, expected_output)
-
-    def test_to_dictionary(self):
-        """Test the distionary"""
-        r1 = Rectangle(1, 2, 1, 1, 4)
-        expected_output = {'id': 4, 'width': 1, 'height': 2, 'x': 1, 'y': 1}
-
-        dict_1 = Rectangle.to_dictionary(r1)
-
-        self.assertEqual(dict_1, expected_output)
-
-    def test_update_1(self):
-        """Test update() in Rectangle"""
-        Base._Base__nb_objects = 0
-        r1 = Rectangle(10, 10, 10, 10)
-        expected_output = '[Rectangle] (1) 10/10 - 10/10\n'
-
-        original_stdout = sys.stdout
-        sys.stdout = StringIO()
-        
-        r1.update()
-        print(r1)
-        update_output = sys.stdout.getvalue()
-        
-        sys.stdout = original_stdout
-        self.assertEqual(update_output, expected_output)
-
-    def test_update_2(self):
-        """Test update(89) in Rectangle"""
-        r1 = Rectangle(10, 10, 10, 10)
-        expected_output = '[Rectangle] (89) 10/10 - 10/10\n'
-
-        original_stdout = sys.stdout
-        sys.stdout = StringIO()
-
-        r1.update(89)
-        print(r1)
-        update_output = sys.stdout.getvalue()
-
-        sys.stdout = original_stdout
-        self.assertEqual(update_output, expected_output)
-
-    def test_update_3(self):
-        """Test update(89, 1) in Rectangle"""
-        r1 = Rectangle(10, 10, 10, 10)
-        expected_output = '[Rectangle] (89) 10/10 - 1/10\n'
-
-        original_stdout = sys.stdout
-        sys.stdout = StringIO()
-
-        r1.update(89, 1)
-        print(r1)
-        update_output = sys.stdout.getvalue()
-
-        sys.stdout = original_stdout
-        self.assertEqual(update_output, expected_output)
-
-    def test_update_4(self):
-        """Test update(89, 1, 2) in Rectangle"""
-        r1 = Rectangle(10, 10, 10, 10)
-        expected_output = '[Rectangle] (89) 10/10 - 1/2\n'
-
-        original_stdout = sys.stdout
-        sys.stdout = StringIO()
-
-        r1.update(89, 1, 2)
-        print(r1)
-        update_output = sys.stdout.getvalue()
-
-        sys.stdout = original_stdout
-        self.assertEqual(update_output, expected_output)
-
-    def test_update_5(self):
-        """Test update(89, 1, 2, 3) in Rectangle"""
-        r1 = Rectangle(10, 10, 10, 10)
-        expected_output = '[Rectangle] (89) 3/10 - 1/2\n'
-
-        original_stdout = sys.stdout
-        sys.stdout = StringIO()
-
-        r1.update(89, 1, 2, 3)
-        print(r1)
-        update_output = sys.stdout.getvalue()
-
-        sys.stdout = original_stdout
-        self.assertEqual(update_output, expected_output)
-
-    def test_update_6(self):
-        """Test update(89, 1, 2, 3, 4) in Rectangle"""
-        r1 = Rectangle(10, 10, 10, 10)
-        expected_output = '[Rectangle] (89) 3/4 - 1/2\n'
-
-        original_stdout = sys.stdout
-        sys.stdout = StringIO()
-
-        r1.update(89, 1, 2, 3, 4)
-        print(r1)
-        update_output = sys.stdout.getvalue()
-
-        sys.stdout = original_stdout
-        self.assertEqual(update_output, expected_output)
-    
-    def test_save_to_file1(self):
-        """Test save_to_file() in Rectangle"""
-        Rectangle.save_to_file(None)
-        
-        with open("Rectangle.json", "r") as f:
-            list_output = json.load(f)
-        
-        expected_output = []
-        self.assertEqual(list_output, expected_output)
-    
-    def test_save_to_file2(self):
-        """Test save_to_file([]) in Rectangle"""
-        filename = "Rectangle.json"
-
+    def test_22(self):
         Rectangle.save_to_file([])
-        self.assertTrue(os.path.exists(filename))
-        
-        with open(filename, "r") as f:
-            file_content = f.read()
 
-        self.assertEqual(file_content, "[]")
-        
-        os.remove(filename)
-    
-    def test_save_to_file_3(self):
-        """Test save_to_file() in Rectangle"""
-        Base._Base__nb_objects = 0
-        Rectangle.save_to_file([Rectangle(1, 2)])
-        
-        with open("Rectangle.json", "r") as f:
-            list_output = json.load(f)
-        
-        expected_output = [{'id': 1, 'width': 1, 'height': 2, 'x': 0, 'y': 0}]
-        self.assertEqual(list_output, expected_output)
-    
-    def test_load_from_file_1(self):
-        """Test load_from_file() in Rectangle doesn't exist"""
-        filename = "nonexistent_file.txt"
+    def test_23(self):
+        dt = Rectangle(1, 2, 3, 4, 5)
+        dt.save_to_file(None)
 
-        with self.assertRaises(TypeError):
-            Rectangle.load_from_file(filename)
-    
-    def test_load_from_file_2(self):
-        """Test load_from_file() in Rectangle exist"""
-        Base._Base__nb_objects = 0
-        Rectangle.save_to_file([Rectangle(1, 2)])
-        list_output = Rectangle.load_from_file()
-        
-        expected_output = [Rectangle(1, 2)]
-        self.assertNotEqual(str(list_output), str(expected_output))
+    def test_25(self):
+        dt = Rectangle(10, 10)
+        dt.area()
 
-if __name__ == "__main__":
-    unittest.main()
+    def test_26(self):
+        dt = Rectangle(1, 1)
+        dt.display()
+
+    def test_27(self):
+        Rectangle.save_to_file(None)
+
+    def test_28(self):
+        dt = Rectangle(1, 2, 3, 4, 5)
+        self.assertEqual(dt.area(), 2)
+
+    def test_29(self):
+        dt = Rectangle(1, 2, 3, 4, 5)
+        self.assertEqual(str(dt), '[Rectangle] (5) 3/4 - 1/2')
+
+    def test_30(self):
+        dt = Rectangle(1, 2, 3, 4)
+        dt.display()
+
+    def test_31(self):
+        dt = Rectangle(1, 2, 3)
+        dt.display()
+
+    def test_32(self):
+        dt = Rectangle(1, 2)
+        dt.display()
