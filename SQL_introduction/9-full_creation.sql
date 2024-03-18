fe2aa344e2660448e067a14d72f4a1f75c06b9d1
tjennat <theo.jennat98@gmail.com>
2024-03-18 20:56:50 +0100

    Script that create a second table with multiple rows

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,13 @@
+-- Script that create a second table with multiple rows
+CREATE TABLE IF NOT EXISTS `second_table`
+(
+    `id` INT,
+    `name` VARCHAR(256),
+    `score` INT
+);
+INSERT INTO `second_table` (`id`, `name`, `score`)
+VALUES
+(1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);
