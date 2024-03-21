bab3c01632da481af34c91df4c6316d49892ea44
tjennat <theo.jennat98@gmail.com>
2024-03-21 17:15:35 +0100

    script that lists all Comedy shows in the database hbtn_0d_tvshows

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,6 @@
+-- script that lists all Comedy shows in the database hbtn_0d_tvshows
+SELECT tv_shows.title
+FROM tv_shows
+JOIN tv_genres ON tv_shows.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title ASC;
