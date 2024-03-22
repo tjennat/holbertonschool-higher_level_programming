3e6187c4d78d35ba049c97329ca1767b38ef138a
tjennat <theo.jennat98@gmail.com>
2024-03-22 01:16:47 +0100

    Script that lists all State objects from the database hbtn_0e_6_usa

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Script that will lists all State objects from the database hbtn_0e_6_usa"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, database))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states_a = (
+        session.query(State)
+        .filter(State.name.like('%a%'))
+        .order_by(State.id)
+    )
+
+    for states in states_a:
+        print("{}: {}".format(states.id, states.name))
+    session.close()
