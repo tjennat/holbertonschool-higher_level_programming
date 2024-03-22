96643dc8b393d082af082f43106566716537c9ea
tjennat <theo.jennat98@gmail.com>
2024-03-22 19:50:36 +0100

    Script that will lists all State objects from the database hbtn_0e_6_usa

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Script that will lists all State objects from the database hbtn_0e_6_usa"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, database))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    # Create a new State object
+    state_name = State(name="Louisiana")
+    session.add(state_name)
+    # Commit the transaction to the database
+    session.commit()
+    print(state_name.id)
+    session.close()
